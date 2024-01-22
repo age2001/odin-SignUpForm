3ea6fea529b72dd521f8434bbc6b4e11ca54e566
age2001 <alan-enriquez@live.com>
2024-01-22 14:22:45 -0600

    Create error msg css class and finish logic for non matching password field error

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let mainForm = document.querySelector("form");
 let passwordField = document.getElementById("password");
 let confirmPasswordField = document.getElementById("confirmpassword");
+let notMatchingPasswordMsg = document.createElement("p");
+notMatchingPasswordMsg.textContent = "* Passwords do not match!"
+notMatchingPasswordMsg.classList.add("tiny-text");
 
 mainForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (passwordField !== confirmPasswordField) {
+    if (passwordField.value !== confirmPasswordField.value) {
         passwordField.style["border-color"] = "red";
         confirmPasswordField.style["border-color"] = "red";
+        if (mainForm.querySelector(".tiny-text") == null) {
+            mainForm.appendChild(notMatchingPasswordMsg);
+        }
     } else {
         mainForm.submit();
     }
